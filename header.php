f1ecc9d8e2dae27e0d427c1078b376f82a10e7d8
Steph Mich <footeuz@gmail.com>
2021-05-23 22:30:16 +0200

    Site meta title

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Zone <?= $mainartist ?> ♫ <?= addslashes($page_title) ?></title>
+    <title><?= TITLE ?> ♫ <?= addslashes($page_title) ?></title>
     <meta charset="UTF-8" />
     <meta name="description" content="<?= addslashes($page_metadesc) ?> ♪" />
     <meta name="viewport" content="width=device-width, user-scalable=no" />
