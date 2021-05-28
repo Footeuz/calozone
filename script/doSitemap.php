73536e7ede3b40a51d2601d08e20eb68e93b55e9
Steph Mich <footeuz@gmail.com>
2021-05-29 00:42:05 +0200

    Sitemap with bdd menu's items

--- script/doSitemap.php
+++ script/doSitemap.php
@@ -3,6 +3,78 @@ require_once '../boot.php';
 error_reporting(E_ALL);
 ini_set('display_errors','on');
 
+$list_contributor = Contributor::getStack();
+
+function doSublink($menuitem) {
+    global $fp;
+    global $mainartistslug;
+
+    $list_podcast = Podcast::getStack(ARTISTID_MAIN);
+    $list_tour = Tour::getStack();
+    $list_pagephotos = PicturePage::getStack('id ASC');
+    $list_disc_cd = Disc::getStack(Disc::SUPPORT_CD, 'is_main DESC, name ASC');
+    $list_disc_single = Disc::getStack(Disc::SUPPORT_SINGLE, 'is_main DESC, name ASC');
+    $list_disc_vinyle = Disc::getStack(Disc::SUPPORT_VINYLE, 'is_main DESC, name ASC');
+    $list_disc_video = Disc::getStack(Disc::SUPPORT_DVD, 'is_main DESC, name ASC');
+    $list_song = Song::getStack('artist_id ASC, name ASC');
+    $list_salles = Salle::getStackArtist();
+
+    if ($menuitem['link_url'] == URL . 'podcast-list' && !empty($list_podcast)) {
+        foreach ($list_podcast as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'podcast-details-' . $item['id'], 'weekly', '0.8'));
+        }
+    }
+
+    if ($menuitem['link_url'] == URL . 'discographie' && !empty($list_disc_cd)) {
+        foreach ($list_disc_cd as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'disc-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.8'));
+        }
+    }
+
+    if ($menuitem['link_url'] == URL . 'vinyles' && !empty($list_disc_vinyle)) {
+        foreach ($list_disc_vinyle as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'disc-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.7'));
+        }
+    }
+
+    if ($menuitem['link_url'] == URL . 'singles' && !empty($list_disc_single)) {
+        foreach ($list_disc_single as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'disc-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.7'));
+        }
+    }
+
+    if ($menuitem['link_url'] == URL . 'videographie' && !empty($list_disc_video)) {
+        foreach ($list_disc_video as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'disc-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.7'));
+        }
+    }
+
+    if ($menuitem['link_url'] == URL . 'chansons-' . $mainartistslug && !empty($list_song)) {
+        foreach ($list_song as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'chanson-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.7'));
+        }
+    }
+
+    if ($menuitem['link_url'] == URL . 'tournees-' . $mainartistslug && !empty($list_tour)) {
+        foreach ($list_tour as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'tournee-tour-' . $item['slug'] . '-' . $item['id'], 'yearly', '0.7'));
+        }
+    }
+
+    if ($menuitem['link_url'] == URL . 'salles-concerts-' . $mainartistslug && !empty($list_salles)) {
+        foreach ($list_salles as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'salle-concert-'.$mainartistslug.'-'. $item['dpt'] .'-'. $item['id'], 'yearly', '0.5'));
+        }
+    }
+
+    if ($menuitem['link_url'] == URL . 'photos' && !empty($list_pagephotos)) {
+        foreach ($list_pagephotos as $item) {
+            fputs($fp, Render::xmlSitemap(URL.'album-photo-' . $item['slug'].'-' . $item['id'], 'yearly', '0.9'));
+        }
+    }
+}
+
+
 $fp = fopen('../sitemap.xml', 'w');
 if ($fp) {
     fputs($fp, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\r\n");
@@ -10,84 +82,31 @@ if ($fp) {
 }
 
 fputs($fp, Render::xmlSitemap(URL , 'yearly', '1'));
-fputs($fp, Render::xmlSitemap(URL . 'podcast-list', 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'salles-concerts-'.$mainartistslug, 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'tournees-'.$mainartistslug, 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'discographie', 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'vinyles', 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'singles', 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'videographie', 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'chansons-'.$mainartistslug, 'yearly', '0.9'));
+
+if (isset($menu_header) && !empty($menu_header)) {
+    foreach ($menu_header as $menu_item) {
+
+        if (!empty($menu_item['link_url'])) {
+            fputs($fp, Render::xmlSitemap(URL . $menu_item['link_url'], 'yearly', '0.9'));
+        }
+        doSublink($menu_item);
+
+        if (!empty($menu_item['childs'])) {
+            foreach ($menu_item['childs'] as $child) {
+                if (!empty($child['link_url'])) {
+                    fputs($fp, Render::xmlSitemap(URL . $child['link_url'], 'yearly', '0.9'));
+                }
+                doSublink($child);
+            }
+        }
+
+    }
+}
+
 fputs($fp, Render::xmlSitemap(URL . 'liste-auteurs-compositeurs-'.$mainartistslug, 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'clip-list', 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'media-list', 'yearly', '0.9'));
-fputs($fp, Render::xmlSitemap(URL . 'recompenses', 'yearly', '0.9'));
-
-fputs($fp, Render::xmlSitemap(URL . 'photos', 'yearly', '0.9'));
-$list_pagephotos = PicturePage::getStack('');
-if (!empty($list_pagephotos)) {
-    foreach($list_pagephotos as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'album-photo-' . $item['slug'].'-' . $item['id'], 'yearly', '0.9'));
-    }
-}
-
-$list_podcast = Podcast::getStack('1');
-if (!empty($list_podcast)) {
-    foreach($list_podcast as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'podcast-details-' . $item['id'], 'weekly', '0.8'));
-    }
-}
-
-$list_disc = Disc::getStack(Disc::SUPPORT_CD);
-if (!empty($list_disc)) {
-    foreach($list_disc as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'disc-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.8'));
-    }
-}
-$list_disc = Disc::getStack(Disc::SUPPORT_SINGLE);
-if (!empty($list_disc)) {
-    foreach($list_disc as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'disc-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.7'));
-    }
-}
-$list_disc = Disc::getStack(Disc::SUPPORT_DVD);
-if (!empty($list_disc)) {
-    foreach($list_disc as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'disc-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.7'));
-    }
-}
-$list_disc = Disc::getStack(Disc::SUPPORT_VINYLE);
-if (!empty($list_disc)) {
-    foreach($list_disc as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'disc-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.7'));
-    }
-}
-
-$list_song = Song::getStack();
-if (!empty($list_song)) {
-    foreach($list_song as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'chanson-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.7'));
-    }
-}
-
-$list_contributor = Contributor::getStack();
 if (!empty($list_contributor)) {
     foreach($list_contributor as $item) {
         fputs($fp, Render::xmlSitemap(URL.'auteur-compositeur-' . $item['slug'] . '-' . $item['id'], 'monthly', '0.5'));
-    }
-}
-
-$list_tour = Tour::getStack();
-if (!empty($list_tour)) {
-    foreach($list_tour as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'tournee-tour-' . $item['slug'] . '-' . $item['id'], 'yearly', '0.7'));
-    }
-}
-
-$list_salles = Salle::getStackArtist();
-if (!empty($list_salles)) {
-    foreach($list_salles as $item) {
-        fputs($fp, Render::xmlSitemap(URL.'salle-concert-'.$mainartistslug.'-'. $item['dpt'] .'-'. $item['id'], 'yearly', '0.5'));
     }
 }
 
--- sitemap.php
+++ sitemap.php
@@ -8,16 +8,94 @@ $og_url = 'plan-du-site';
 $og_image = 'public/images/tournees-'.$mainartistslug.'.jpg';
 $og_type = '';
 
-$list_podcast = Podcast::getStack('1');
-$list_tour = Tour::getStack();
-$list_pagephotos = PicturePage::getStack('id ASC');
-$list_disc_cd = Disc::getStack(Disc::SUPPORT_CD, 'is_main DESC, name ASC');
-$list_disc_single = Disc::getStack(Disc::SUPPORT_SINGLE, 'is_main DESC, name ASC');
-$list_disc_vinyle = Disc::getStack(Disc::SUPPORT_VINYLE, 'is_main DESC, name ASC');
-$list_disc_video = Disc::getStack(Disc::SUPPORT_DVD, 'is_main DESC, name ASC');
-$list_song = Song::getStack('artist_id ASC, name ASC');
 $list_contributor = Contributor::getStack();
-$list_salles = Salle::getStackArtist();
+
+function doSublink($menuitem) {
+    global $mainartist;
+    global $mainartistslug;
+
+    $list_podcast = Podcast::getStack(ARTISTID_MAIN);
+    $list_tour = Tour::getStack();
+    $list_pagephotos = PicturePage::getStack('id ASC');
+    $list_disc_cd = Disc::getStack(Disc::SUPPORT_CD, 'is_main DESC, name ASC');
+    $list_disc_single = Disc::getStack(Disc::SUPPORT_SINGLE, 'is_main DESC, name ASC');
+    $list_disc_vinyle = Disc::getStack(Disc::SUPPORT_VINYLE, 'is_main DESC, name ASC');
+    $list_disc_video = Disc::getStack(Disc::SUPPORT_DVD, 'is_main DESC, name ASC');
+    $list_song = Song::getStack('artist_id ASC, name ASC');
+    $list_salles = Salle::getStackArtist();
+
+    if ($menuitem['link_url'] == URL . 'podcast-list' && !empty($list_podcast)) {
+        echo '<ul>';
+            foreach ($list_podcast as $item) {
+                echo '<li><a href="' . URL . 'podcast-details-' . $item['id'] . '" title="Podcast ' . $mainartist . ' ' . $item['name'] . '">' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+
+    if ($menuitem['link_url'] == URL . 'discographie' && !empty($list_disc_cd)) {
+        echo '<ul>';
+            foreach ($list_disc_cd as $item) {
+                echo '<li><a href="' . URL . 'disc-' . $item['slug'] . '-' . $item['id'] . '" title="Disque ' . $mainartist . ' ' . $item['name'] . '">' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+
+    if ($menuitem['link_url'] == URL . 'vinyles' && !empty($list_disc_vinyle)) {
+        echo '<ul>';
+            foreach ($list_disc_vinyle as $item) {
+                echo '<li><a href="' . URL . 'disc-' . $item['slug'] . '-' . $item['id'] . '" title="Vinyle ' . $mainartist . ' ' . $item['name'] . '">' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+
+    if ($menuitem['link_url'] == URL . 'singles' && !empty($list_disc_single)) {
+        echo '<ul>';
+            foreach ($list_disc_single as $item) {
+                echo '<li><a href="' . URL . 'disc-' . $item['slug'] . '-' . $item['id'] . '" title="Single ' . $mainartist . ' ' . $item['name'] . '">' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+
+    if ($menuitem['link_url'] == URL . 'videographie' && !empty($list_disc_video)) {
+        echo '<ul>';
+            foreach ($list_disc_video as $item) {
+                echo '<li><a href="' . URL . 'disc-' . $item['slug'] . '-' . $item['id'] . '" title="Vidéo ' . $mainartist . ' ' . $item['name'] . '">' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+
+    if ($menuitem['link_url'] == URL . 'chansons-' . $mainartistslug && !empty($list_song)) {
+        echo '<ul>';
+            foreach ($list_song as $item) {
+                echo '<li><a href="' . URL . 'chanson-' . $item['slug'] . '-' . $item['id'] . '" title="Chanson ' . $mainartist . ' ' . $item['name'] . '">' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+
+    if ($menuitem['link_url'] == URL . 'tournees-' . $mainartistslug && !empty($list_tour)) {
+        echo '<ul>';
+            foreach ($list_tour as $item) {
+                echo '<li><a href="' . URL . 'tournee-' . $item['slug'] . '-' . $item['id'] . '" title="Dates de la tournée de ' . $mainartist . ' ' . $item['name'] . '">' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+
+    if ($menuitem['link_url'] == URL . 'salles-concerts-' . $mainartistslug && !empty($list_salles)) {
+        echo '<ul>';
+            foreach ($list_salles as $item) {
+                echo '<li><a href="' . URL . 'salle-concert-' . $mainartistslug . '-' . $item['dpt'] . '-' . $item['id'] . '" title="Salle concert ' . $mainartist . ' ' . $item['name'] . '">' . strtoupper($item['country']) . ' - ' . $item['city'] . '(' . $item['dpt'] . ') - ' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+
+    if ($menuitem['link_url'] == URL . 'photos' && !empty($list_pagephotos)) {
+        echo '<ul>';
+            foreach ($list_pagephotos as $item) {
+                echo '<li><a href="' . URL . 'album-photo-' . $item['slug'] . '-' . $item['id'] . '" title="Album photo ' . $mainartist . ' ' . $item['name'] . '">' . $item['name'] . '</a>';
+            }
+        echo '</ul>';
+    }
+}
 
 include("header.php");
 ?>
@@ -29,114 +107,35 @@ include("header.php");
             <h2><?= $page_title ?> de <?= $mainartist ?></h2>
             <ul>
                 <li><a href="<?= URL ?>"><?= TITLE ?></a></li>
-                <li><a href="<?= URL ?>podcast-list"><?= $lang->l('podcast_list_title') ?></a>
-                    <?php if (!empty($list_podcast)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_podcast as $item) {
-                                echo '<li><a href="'.URL.'podcast-details-' . $item['id'].'" title="Podcast '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
+                <?php if (isset($menu_header) && !empty($menu_header)) { ?>
+                    <?php foreach ($menu_header as $menu_item) { ?>
+                        <li>
+                            <?php if (!empty($menu_item['link_url'])) { ?><a href="<?= $menu_item['link_url'] ?>"><?= $menu_item['name'] ?></a><?php } else { echo $menu_item['name'];} ?>
+
+                            <?php doSublink($menu_item); ?>
+
+                            <?php if (!empty($menu_item['childs'])) { ?>
+                                <ul>
+                                    <?php foreach ($menu_item['childs'] as $child) { ?>
+                                        <li>
+                                            <?php if (!empty($child['link_url'])) { ?><a href="<?= $child['link_url'] ?>"><?= $child['name'] ?></a><?php } else { echo $child['name']; } ?>
+
+                                            <?php doSublink($child); ?>
+                                        </li>
+                                    <?php } ?>
+                                </ul>
+                            <?php } ?>
+
+                        </li>
                     <?php } ?>
-                </li>
-                <li><a href="<?= URL ?>clip-list"><?= $lang->l('clip_list_title') ?></a></li>
-                <li><a href="<?= URL ?>media-list"><?= $lang->l('media_list_title') ?></a></li>
-                <li><a href="<?= URL ?>recompenses"><?= $lang->l('recompenses_title') ?></a></li>
-                <li><a href="<?= URL ?>photos"><?= $lang->l('pagephotos_list_title') ?></a>
-                    <?php if (!empty($list_pagephotos)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_pagephotos as $item) {
-                                echo '<li><a href="'.URL.'album-photo-' . $item['slug'].'-' . $item['id'].'" title="Album photo '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
-                    <?php } ?>
-                </li>
-                <li><a href="<?= URL ?>discographie"><?= $lang->l('disc_list_title') ?></a>
-                    <?php if (!empty($list_disc_cd)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_disc_cd as $item) {
-                                echo '<li><a href="'.URL.'disc-' . $item['slug'] . '-' . $item['id'].'" title="Disque '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
-                    <?php } ?>
-                </li>
-                <li><a href="<?= URL ?>vinyles"><?= $lang->l('vinyle_list_title') ?></a>
-                    <?php if (!empty($list_disc_vinyle)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_disc_vinyle as $item) {
-                                echo '<li><a href="'.URL.'disc-' . $item['slug'] . '-' . $item['id'].'" title="Vinyle '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
-                    <?php } ?>
-                </li>
-                <li><a href="<?= URL ?>singles"><?= $lang->l('single_list_title') ?></a>
-                    <?php if (!empty($list_disc_single)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_disc_single as $item) {
-                                echo '<li><a href="'.URL.'disc-' . $item['slug'] . '-' . $item['id'].'" title="Single '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
-                    <?php } ?>
-                </li>
-                <li><a href="<?= URL ?>videographie"><?= $lang->l('video_list_title') ?></a>
-                    <?php if (!empty($list_disc_video)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_disc_video as $item) {
-                                echo '<li><a href="'.URL.'disc-' . $item['slug'] . '-' . $item['id'].'" title="Vidéo '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
-                    <?php } ?>
-                </li>
-                <li><a href="<?= URL ?>chansons-<?= $mainartistslug ?>"><?= $lang->l('song_list_title') ?></a>
-                    <?php if (!empty($list_song)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_song as $item) {
-                                echo '<li><a href="'.URL.'chanson-' . $item['slug'] . '-' . $item['id'].'" title="Chanson '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
-                    <?php } ?>
-                </li>
+                <?php } ?>
+
                 <li><a href="<?= URL ?>liste-auteurs-compositeurs-<?= $mainartistslug ?>"><?= $lang->l('contributor_list_title') ?></a>
                     <?php if (!empty($list_contributor)) { ?>
                         <ul>
                             <?php
                             foreach($list_contributor as $item) {
                                 echo '<li><a href="'.URL.'auteur-compositeur-' . $item['slug'] . '-' . $item['id'].'" title="Chanson '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
-                    <?php } ?>
-                </li>
-                <li><a href="<?= URL ?>tournees-<?= $mainartistslug ?>"><?= $lang->l('tour_list_title') ?></a>
-                    <?php if (!empty($list_tour)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_tour as $item) {
-                                echo '<li><a href="'.URL.'tournee-'.$item['slug'].'-'.$item['id'].'" title="Dates de la tournée de '.$mainartist.' '.$item['name'].'">'.$item['name'].'</a>';
-                            }
-                            ?>
-                        </ul>
-                    <?php } ?>
-                </li>
-                <li><a href="<?= URL ?>salles-concerts-<?= $mainartistslug ?>"><?= $lang->l('salle_list_title') ?></a>
-                    <?php if (!empty($list_salles)) { ?>
-                        <ul>
-                            <?php
-                            foreach($list_salles as $item) {
-                                echo '<li><a href="'.URL.'salle-concert-'.$mainartistslug.'-'. $item['dpt'] .'-'. $item['id'].'" title="Salle concert '.$mainartist.' '.$item['name'].'">'. strtoupper($item['country']) .' - '. $item['city'] .'('. $item['dpt'] .') - '. $item['name'] .'</a>';
                             }
                             ?>
                         </ul>
